b523b1120ea75f0f011a84565d5a89b57f738585
Crossley Rozario <crozariodev@gmail.com>
2018-02-24 16:43:11 -0500

    implemented circle button

---
+++ Rewinder/RoundPlayButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundPlayButton.swift
+//  Rewinder
+//
+//  Created by Crossley Rozario on 2/24/18.
+//  Copyright © 2018 Crossley Rozario. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundPlayButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+}
