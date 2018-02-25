5260690f9b1e570019ce8fe42ba55befffcd8800
shahhaard47 <shaard74@gmail.com>
2018-02-25 05:41:41 -0500

    GOT THEM HIGHLIGHTS

--- Rewinder/Audio.swift
+++ Rewinder/Audio.swift
@@ -11,7 +11,6 @@ import AVFoundation
 class Audio {
 	
 	var session: AVAudioSession?
-	var recorder: AVAudioRecorder?
 	
 	let recordSettings = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue, AVEncoderBitRateKey: 16, AVNumberOfChannelsKey: 2, AVSampleRateKey: 44100.0] as [String: Any]
 	
@@ -21,15 +20,18 @@ class Audio {
 	var dataURL: URL?
 	var dataPath: String?
 	
+	var highlightsURL: URL?
+	var highlightsPath: String?
+	
 	var temp1: URL?
 	var temp2: URL?
+	var temp: URL?
 	
 	// is the current file being recorded to first temp?
 	var firstTemp: Bool?
 	
 	// should be one Audio object per app run
 	init() {
-//		recorder = audioRecorder
 		
 		session = AVAudioSession.sharedInstance()
 		do {
@@ -46,7 +48,6 @@ class Audio {
 		docsURL = dirs[0]
 		dataURL = docsURL!.appendingPathComponent("data")
 		dataPath = dataURL!.path
-		
 		if !(filemgr.fileExists(atPath: dataPath!)){
 			do {
 				try filemgr.createDirectory(atPath: dataPath!, withIntermediateDirectories: true, attributes: nil)
@@ -57,8 +58,107 @@ class Audio {
 		}
 		temp1 = dataURL!.appendingPathComponent("temp1.caf")
 		temp2 = dataURL!.appendingPathComponent("temp2.caf")
+		//extra temp file
+		temp = dataURL!.appendingPathComponent("temp.caf")
+		
+		highlightsURL = docsURL!.appendingPathComponent("highlights")
+		highlightsPath = highlightsURL!.path
+		if !(filemgr.fileExists(atPath: highlightsPath!)){
+			do {
+				try filemgr.createDirectory(atPath: highlightsPath!, withIntermediateDirectories: true, attributes: nil)
+			}
+			catch let error {
+				print (error)
+			}
+		}
 		
 		firstTemp = true
+	}
+	
+	func mergeAndAddHighlight(_ file1: URL, _ file2: URL,_ file3: URL) {
+		// Create a new audio track we can append to
+		let composition = AVMutableComposition()
+		var appendedAudioTrack: AVMutableCompositionTrack? = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+		// Grab the two audio tracks that need to be appended
+		let asset1 = AVURLAsset(url: URL(fileURLWithPath: file1.path), options: nil)
+		let asset2 = AVURLAsset(url: URL(fileURLWithPath: file2.path), options: nil)
+		let asset3 = AVURLAsset(url: URL(fileURLWithPath: file3.path), options: nil)
+		var error: Error? = nil
+		// Grab the first audio track and insert it into our appendedAudioTrack
+		var track1 = asset1.tracks(withMediaType: .audio) as [AVAssetTrack]
+		var timeRange: CMTimeRange = CMTimeRangeMake(kCMTimeZero, asset1.duration)
+		if let aIndex = track1[0] as? AVAssetTrack {
+			try? appendedAudioTrack?.insertTimeRange(timeRange, of: aIndex, at: kCMTimeZero)
+		}
+		if error != nil {
+			// do something
+			return
+		}
+		// Grab the second audio track and insert it at the end of the first one
+		var track2 = asset2.tracks(withMediaType: .audio) as [AVAssetTrack]
+		timeRange = CMTimeRangeMake(kCMTimeZero, asset2.duration)
+		if let aIndex = track2[0] as? AVAssetTrack {
+			try? appendedAudioTrack?.insertTimeRange(timeRange, of: aIndex, at: asset1.duration)
+		}
+		if error != nil {
+			// do something
+			return
+		}
+		// Grab the third audio track and insert it at the end of the second one
+		var track3 = asset3.tracks(withMediaType: .audio) as [AVAssetTrack]
+		timeRange = CMTimeRangeMake(kCMTimeZero, asset3.duration)
+		if let aIndex = track3[0] as? AVAssetTrack {
+			try? appendedAudioTrack?.insertTimeRange(timeRange, of: aIndex, at: asset2.duration)
+		}
+		if error != nil {
+			// do something
+			return
+		}
+		
+		// Create a new audio file using the appendedAudioTrack
+		let exportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetPassthrough)
+		if exportSession == nil {
+			// do something
+			return
+		}
+//		let fileName = getDatetimeString()
+		let fileName: String = "littHighlight3.caf"
+		print("filename \(fileName)")
+		let appendedAudioPath = highlightsURL?.appendingPathComponent(fileName)
+		
+		//remove if exists
+		if FileManager.default.fileExists(atPath: appendedAudioPath!.path) {
+			do {
+				try FileManager.default.removeItem(at: appendedAudioPath!)
+			} catch let error {
+				print (error)
+			}
+		}
+		
+		// make sure to fill this value in
+		exportSession?.outputURL = appendedAudioPath
+		exportSession?.outputFileType = AVFileType.caf
+		exportSession?.exportAsynchronously(completionHandler: {() -> Void in
+			// exported successfully?
+			switch exportSession?.status {
+			case .failed?:
+				print("failed")
+				break
+			case .completed?:
+				// you should now have the appended audio file
+				print("SUCCESS")
+				break
+			case .waiting?:
+				break
+			default:
+				break
+			}
+			var _: Error? = nil
+		})
+	}
+	
+	func getDatetimeString() ->String {
+		
 	}
 	
 	func requestPermission(_ session: AVAudioSession) {
@@ -87,21 +187,12 @@ class Audio {
 		return soundFile!
 	}
 	
-//	func startRecording(recorder: AVAudioRecorder) {
-//
-//		recorder?.record(forDuration: 3.0)
-//
-//	}
+	
+	
 	
 	func getDocumentsDirectory() -> URL {
 		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 		return paths[0]
-	}
-	
-	func stopRecording() {
-		if recorder?.isRecording == true {
-			recorder?.stop()
-		}
 	}
 	
 //	func trimTillEnd(inputFile, timeFromEnd)
@@ -115,6 +206,22 @@ class Audio {
 //			print(files)
 			for file in files {
 				fileURLs.append((dataURL?.appendingPathComponent(file))!)
+			}
+		}
+		catch let error {
+			print (error)
+		}
+		return fileURLs
+	}
+	
+	func listOfHighlights() -> [URL] {
+		var fileURLs = [URL]()
+		do {
+			let files = try filemgr.contentsOfDirectory(atPath: highlightsPath!)
+			//			print(files)
+			for file in files {
+//				fileURLs.append((highlightsURL?.appendingPathComponent(file))!)
+				fileURLs.append(URL(string: file)!)
 			}
 		}
 		catch let error {
--- Rewinder/HomeViewController.swift
+++ Rewinder/HomeViewController.swift
@@ -16,6 +16,8 @@ class HomeViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlay
 	var audioPlayer: AVAudioPlayer?
 	var audioRecorder: AVAudioRecorder?
 	
+	let recordDuration: Double = 6.0
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,37 +30,205 @@ class HomeViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlay
 
 	func beginRecording() {
 		let recordFile = audioObj!.getNextTempFile()
-		print("recording to " + recordFile.path)
+//		print("recording to " + recordFile.path)
 		do {
 			try audioRecorder = AVAudioRecorder(url: recordFile, settings: audioObj?.recordSettings as! [String: AnyObject])
 			audioRecorder?.delegate = self
 			audioRecorder?.prepareToRecord()
-			let recordStatus = audioRecorder?.record(forDuration: 3.0)
-			print(recordStatus!)
+//			print("recording starts: \(recordFile)")
+			let recordStatus = audioRecorder?.record(forDuration: recordDuration)
+//			print(recordStatus!)
+			
+		}catch let error {
+			print (error)
+		}
+	}
+	
+	func altRecording(file3: URL) {
+		let recordFile = file3
+		do {
+			try audioRecorder = AVAudioRecorder(url: recordFile, settings: audioObj?.recordSettings as! [String: AnyObject])
+			audioRecorder?.delegate = self
+			audioRecorder?.prepareToRecord()
+			print("SPECIAL recording starts:")
+			let recordStatus = audioRecorder?.record(forDuration: recordDuration)
+//			print(recordStatus!)
+			
 		}catch let error {
 			print (error)
 		}
 	}
 	
 	@IBAction func addHighlight(_ sender: RoundPlayButton) {
-		let files = audioObj?.listOfAudioFiles()
-		for file in files!{
-//			print(file.path)
+		
+//		let files = audioObj?.listOfAudioFiles()
+//		for file in files!{
+//			do {
+//				try audioPlayer = AVAudioPlayer(contentsOf: file)
+//				audioPlayer?.delegate = self
+//				audioPlayer?.prepareToPlay()
+//				audioPlayer?.play()
+//			} catch let error as NSError {
+//				print("audioPlayer error \(error.localizedDescription)")
+//			}
+//		}
+		computeHighlight()
+//		let files = audioObj?.listOfHighlights()
+//		for file in files!{
 //			print(file)
-			do {
-				try audioPlayer = AVAudioPlayer(contentsOf: file)
-				audioPlayer?.delegate = self
-				audioPlayer?.prepareToPlay()
-				let playStatus = audioPlayer?.play()
-			} catch let error as NSError {
-				print("audioPlayer error \(error.localizedDescription)")
-			}
+//			do {
+//				try audioPlayer = AVAudioPlayer(contentsOf: file)
+//				audioPlayer?.delegate = self
+//				audioPlayer?.prepareToPlay()
+//				audioPlayer?.play()
+//			} catch let error as NSError {
+//				print("audioPlayer error \(error.localizedDescription)")
+//			}
+//		}
+	}
+	
+	var high1: URL?
+	var trimmedHigh1: URL?
+	var high2: URL?
+	var new3: URL?
+	var cropTime: TimeInterval?
+	
+	func computeHighlight(){
+		//get current recording time
+		cropTime = audioRecorder?.currentTime
+		
+		//stop recording
+		audioRecorder?.stop()
+		
+		new3 = audioObj?.temp
+		altRecording(file3: new3!)
+		
+		//which temp lastest?
+		if audioObj?.firstTemp == true {
+			// temp2 latest
+			high2 = audioObj?.temp2
+			high1 = audioObj?.temp1
+		}
+		else {
+			// temp1 latest
+			high2 = audioObj?.temp1
+			high1 = audioObj?.temp2
+		}
+		if high1 != nil {
+			//need to trip
+			let asset = AVAsset(url: high1!)
+			trimmedHigh1 = audioObj?.dataURL?.appendingPathComponent("trimmed.caf")
+			exportAsset(asset, trimmedSoundFileURL: trimmedHigh1!)
 		}
 	}
 	
+	func stitchHighlight(currTime: TimeInterval, trimmed: URL, file2: URL, file3: URL) {
+		// then merge all files
+		audioObj?.mergeAndAddHighlight(trimmed, file2, file3)
+	}
+	
 	func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-//		audioObj?.startRecording()
-		print("delegate Called")
-		beginRecording()
+
+//		let files = audioObj?.listOfAudioFiles()
+//		for file in files!{
+//			//			print(file.path)
+//			//			print(file)
+//			do {
+//				try audioPlayer = AVAudioPlayer(contentsOf: file)
+//				audioPlayer?.delegate = self
+//				audioPlayer?.prepareToPlay()
+//				//				let playStatus = audioPlayer?.play()
+//				print(file)
+//				print(audioPlayer?.duration)
+//			} catch let error as NSError {
+//				print("audioPlayer error \(error.localizedDescription)")
+//			}
+//		}
+		
+		if new3 != nil {
+			if recorder.url == new3! {
+				//stitch
+				stitchHighlight(currTime: cropTime!, trimmed: trimmedHigh1!, file2: high2!, file3: new3!)
+				
+				let list = [trimmedHigh1, high2, new3, audioObj?.highlightsURL?.appendingPathComponent("littHighlight3.caf")]
+				for file in list{
+					if file != nil {
+						do {
+							try audioPlayer = AVAudioPlayer(contentsOf: file!)
+							audioPlayer?.delegate = self
+							audioPlayer?.prepareToPlay()
+							//				let playStatus = audioPlayer?.play()
+							print(file)
+							print(audioPlayer?.duration)
+						} catch let error as NSError {
+							print("audioPlayer error \(error.localizedDescription)")
+						}
+					}
+				}
+
+				new3 = nil
+//				beginRecording()
+			}
+		}
+		else {
+			beginRecording()
+		}
+	}
+	
+	func exportAsset(_ asset: AVAsset, trimmedSoundFileURL: URL) {
+		print("\(#function)")
+		
+//		let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//		let trimmedSoundFileURL = documentsDirectory.appendingPathComponent(fileName)
+		print("saving to \(trimmedSoundFileURL.absoluteString)")
+		
+		if FileManager.default.fileExists(atPath: trimmedSoundFileURL.absoluteString) {
+			print("sound exists, removing \(trimmedSoundFileURL.absoluteString)")
+			do {
+				if try trimmedSoundFileURL.checkResourceIsReachable() {
+					print("is reachable")
+				}
+				
+				try FileManager.default.removeItem(atPath: trimmedSoundFileURL.absoluteString)
+			} catch {
+				print("could not remove \(trimmedSoundFileURL)")
+				print(error.localizedDescription)
+			}
+			
+		}
+		
+		print("creating export session for \(asset)")
+		
+		if let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetPassthrough) {
+			exporter.outputFileType = AVFileType.caf
+			exporter.outputURL = trimmedSoundFileURL
+			
+//			let duration = CMTimeGetSeconds(asset.duration)
+
+			let startTime = CMTimeMakeWithSeconds(cropTime!, 1000000)
+			exporter.timeRange = CMTimeRangeFromTimeToTime(startTime, kCMTimePositiveInfinity)
+//			exporter.timeRange = CMTimeRangeFromTimeToTime(startTime, stopTime)
+			
+			// do it
+			exporter.exportAsynchronously(completionHandler: {
+				print("export complete \(exporter.status)")
+				
+				switch exporter.status {
+				case  AVAssetExportSessionStatus.failed:
+					
+					if let e = exporter.error {
+						print("export failed \(e)")
+					}
+					
+				case AVAssetExportSessionStatus.cancelled:
+					print("export cancelled \(String(describing: exporter.error))")
+				default:
+					print("export complete")
+				}
+			})
+		} else {
+			print("cannot create AVAssetExportSession for asset \(asset)")
+		}
+		
 	}
 }
