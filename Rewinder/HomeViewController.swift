a2d57d3c9221162b71cff613c7e2b46212875dc2
Crossley Rozario <crozariodev@gmail.com>
2018-02-25 06:31:43 -0500

    revised HomeViewController

--- Rewinder/HomeViewController.swift
+++ Rewinder/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import DSWaveformImage
 
 class HomeViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
@@ -26,6 +27,30 @@ class HomeViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlay
 		
 //		audioObj?.startRecording()
 		self.beginRecording()
+        
+        
+        let guide = view.safeAreaLayoutGuide
+        let height = guide.layoutFrame.size.height
+        let viewWidth = view.bounds.size.width
+        
+        let waveformImageDrawer = WaveformImageDrawer()
+        let audioURL = Bundle.main.url(forResource: "Eminem - Rap God", withExtension: "mp3")!
+        let topWaveformImage = waveformImageDrawer.waveformImage(fromAudioAt: audioURL,
+                                                                         size: UIScreen.main.bounds.size,
+                                                                         color: UIColor.blue,
+                                                                         backgroundColor: UIColor.lightGray,
+                                                                         style: .striped,
+                                                                         position: .middle,
+                                                                         scale: UIScreen.main.scale)
+        let waveform = Waveform(audioAssetURL: audioURL)!
+    
+        
+        let image = topWaveformImage
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 50, width: viewWidth, height: viewWidth/2)
+        view.addSubview(imageView)
+        print("so many samples: \(waveform.samples(count: 200))")
+        
     }
 
 	func beginRecording() {
